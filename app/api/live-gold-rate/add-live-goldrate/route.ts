dc2c7d0bc75b70f43f908a7eb4c2b7f9e36d4f01
Hamza Waqas <hamza@creationnext.com>
2024-08-27 21:31:05 +0500

    Update route.ts

--- app/api/live-gold-rate/add-live-goldrate/route.ts
+++ app/api/live-gold-rate/add-live-goldrate/route.ts
@@ -883,6 +883,7 @@
 import { apiBaseUrl } from "@/context/constants";
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 async function fetchRates() {
   try {
